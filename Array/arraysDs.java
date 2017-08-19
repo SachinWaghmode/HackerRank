b8aedb95a39c8b54ae9b3091a25d07067f98e07c
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-08-19 13:41:32 -0700

    Update arraysDs.java

--- Array/arraysDs.java
+++ Array/arraysDs.java
@@ -26,3 +26,27 @@ Sample Output
 2 3 4 1
 
 */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter the number of elements in Array");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        //System.out.println("Enter the elements in Array");
+        for( int i=0; i < n; i++){
+            array[i] = sc.nextInt();
+        }
+        //System.out.println("Desired Output: ");
+        for( int j=n-1; j >= 0 ; j--){
+            System.out.print(array[j]+" ");
+        }
+            
+    }
+}
