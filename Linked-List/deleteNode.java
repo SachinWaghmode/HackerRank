009874c44799fedc1f5a68a970e7a7ea90004357
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-09-08 23:56:53 -0700

    Create deleteNode.java

---
+++ Linked-List/deleteNode.java
@@ -0,0 +1,14 @@
+/*
+Sample Input
+
+1 --> 2 --> 3 --> NULL, position = 0 
+1 --> NULL , position = 0
+
+Sample Output
+
+2 --> 3 --> NULL
+NULL
+Explanation 
+1. 0th position is removed, 1 is deleted from the list. 
+2. Again 0th position is deleted and we are left with empty list.
+*/
