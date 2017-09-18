4a198f52dc293b6717d68c8e1840b57f687707dc
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-09-18 02:02:12 -0700

    Update cycleDetection.java

--- Linked-List/cycleDetection.java
+++ Linked-List/cycleDetection.java
@@ -43,3 +43,19 @@ boolean hasCycle(Node head) {
       }
       return false;    
 }
+
+
+/* Recommended Solution with O(1)
+public boolean hasCycle(ListNode head) {
+    if(head==null) return false;
+    ListNode walker = head;
+    ListNode runner = head;
+    while(runner.next!=null && runner.next.next!=null) {
+        walker = walker.next;
+        runner = runner.next.next;
+        if(walker==runner) return true;
+    }
+    return false;
+}
+*/
+
