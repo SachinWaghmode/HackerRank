90a3961230b67543b03282ba9a15077dcf2ebf77
Sachin Dadaso Waghmode <sachinwaghmode57@gmail.com>
2018-03-03 22:04:14 -0800

    Update nodeValue.java

--- Linked-List/nodeValue.java
+++ Linked-List/nodeValue.java
@@ -19,3 +19,24 @@ Sample Output
 6
 3
 */
+/*
+  Get Nth element from the end in a linked list of integers
+  Number of elements in the list will always be greater than N.
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    
+int GetNode(Node head,int n) {
+     // This is a "method-only" submission. 
+     // You only need to complete this method. 
+      List<Integer> nodes = new ArrayList<>();
+      if (head == null) return 0;
+      while(head != null){
+          nodes.add(head.data);
+          head = head.next;
+      }
+    return nodes.get(nodes.size() - n-1);
+}
