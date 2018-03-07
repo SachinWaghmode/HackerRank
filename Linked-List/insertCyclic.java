b524f1b5f340e9e9e4f5deb2521d994ce70d99f5
Sachin Dadaso Waghmode <sachinwaghmode57@gmail.com>
2018-03-07 00:43:26 -0800

    Update insertCyclic.java

--- Linked-List/insertCyclic.java
+++ Linked-List/insertCyclic.java
@@ -7,6 +7,44 @@ Example:
       \   /
         22
 */
-public static void insertNode(Node node, int value){
-        
-}
+public static void insertNodeSorted(Node node, int value){
+       
+        Node head = node;
+        if (node == null) {
+            node = new Node(x);
+            node.next = node;
+            return node;
+        }
+        if (node.next == head) {
+            insertNode(node, x);
+            return head;
+        }
+        while (node != null && node.next != null) {
+            if (node.val < node.next.val) {
+                if (node.val <= x && x <= node.next.val) {
+                    insertNode(node, x);
+                    break;
+                }
+            } else if (node.val > node.next.val) {
+                if (x >= node.val || x <= node.next.val) {
+                    insertNode(node, x);
+                    break;
+                }
+            } else {
+                if (node.next == head) {
+                    insertNode(node, x);
+                    break;
+                }
+            }
+            node = node.next;
+        }
+        return head;
+    }
+
+    public void insertNode(Node head, int x) {
+        Node node = new Node(x);
+        node.next = head.next;
+        head.next = node;
+    }
+
+
