40078e651087518984816a2d0c385012f06ffe12
Sachin Dadaso Waghmode <sachinwaghmode57@gmail.com>
2018-03-14 22:24:37 -0700

    Update leftRotate.java

--- Array/leftRotate.java
+++ Array/leftRotate.java
@@ -27,3 +27,49 @@ When we perform  left rotations, the array undergoes the following sequence of c
 
 Thus, we print the array's final state as a single line of space-separated values, which is 5 1 2 3 4.
 */
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] nd = scan.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0].trim());
+
+        int d = Integer.parseInt(nd[1].trim());
+
+        int[] a = new int[n];
+
+        String[] aItems = scan.nextLine().split(" ");
+
+        for (int aItr = 0; aItr < n; aItr++) {
+            int aItem = Integer.parseInt(aItems[aItr].trim());
+            a[aItr] = aItem;
+        }
+        int[] r = leftRotate(a,d);
+            for(int i=0; i<n; i++){
+                System.out.print(r[i]+" ");
+            }
+    }
+    
+    public static int[] leftRotate(int[] a, int d){
+        int count =0, j=0;
+        if(d%a.length==0) return a;
+        while(count<d){
+            int temp = a[0];
+            for (j=0; j<a.length-1; j++)
+            {  
+                a[j] = a[j+1];
+            }
+            a[j] = temp;
+            count++;
+        }
+        return a;
+    }
+}
