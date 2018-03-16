74d75808f283122d4a72086121d7adbe26f60f61
Sachin Dadaso Waghmode <sachinwaghmode57@gmail.com>
2018-03-16 02:48:20 -0700

    Update lca.java

--- Trees/lca.java
+++ Trees/lca.java
@@ -42,3 +42,21 @@ Sample Output
 LCA of  and  is  (which is the root). 
 Return a pointer to the root in this case.
 */
+
+/* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+static Node lca(Node root,int v1,int v2)
+    {
+      if(v1<root.data && v2>root.data) return root;
+      if(v1<root.data && v2<root.data) return lca(root.left,v1,v2);
+      if (v1>root.data && v2>root.data) return lca(root.right,v1,v2);
+     return root;  
+    }
+
+
