81206b9e60f0010339495e5088699dd363735d81
Sachin Dadaso Waghmode <sachinwaghmode57@gmail.com>
2018-03-24 23:50:16 -0700

    Create frequencySort.java

---
+++ Array/frequencySort.java
@@ -0,0 +1,21 @@
+/* 
+Code Pair problem:
+Given a text containing words, spaces, new lines and punctuations, 
+Print the frequency of each word ignoring capitalization in order of frequency.
+
+Sample input: "The quick brown fox, jumped over the brown log. The slow red fox did not.
+
+output:
+the: 3
+brown: 2
+fox: 2
+over: 1
+red: 1
+quick: 1
+not: 1
+slow: 1
+log: 1
+jumped: 1
+did: 1
+
+*/
